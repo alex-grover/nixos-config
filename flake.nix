5f56f99485ae2c3ca674d167e8ed85c6e3e8d4cd
Alex Grover <hello@alexgrover.me>
2026-02-24 07:05:49 -0500

    Add macos system preferences

--- flake.nix
+++ flake.nix
@@ -70,6 +70,58 @@
                   cleanup = "zap";
                 };
               };
+
+              system.startup.chime = false;
+              system.defaults = {
+                CustomUserPreferences = {
+                  AppleFirstWeekday = {
+                    gregorian = 2;
+                  };
+                  "com.apple.Safari" = {
+                    IncludeDevelopMenu = true;
+                    ShowFavoritesBar = false;
+                  };
+                  "com.apple.menuextra.clock" = {
+                    Show24Hour = true;
+                  };
+                };
+                NSGlobalDomain = {
+                  AppleFontSmoothing = 0;
+                  AppleInterfaceStyleSwitchesAutomatically = true;
+                  KeyRepeat = 1;
+                  InitialKeyRepeat = 30;
+                  "com.apple.swipescrolldirection" = false;
+                  "com.apple.trackpad.scaling" = 2.0;
+                };
+                ".GlobalPreferences"."com.apple.mouse.scaling" = 1.5;
+                controlcenter = {
+                  BatteryShowPercentage = false;
+                  NowPlaying = false;
+                };
+                dock = {
+                  autohide = true;
+                  mru-spaces = false;
+                  showMissionControlGestureEnabled = true;
+                  show-recents = false;
+                  wvous-br-corner = 2;
+                  wvous-tl-corner = 10;
+                  wvous-tr-corner = 4;
+                };
+                finder = {
+                  FXPreferredViewStyle = "clmv";
+                  ShowExternalHardDrivesOnDesktop = false;
+                  ShowRemovableMediaOnDesktop = false;
+                };
+                trackpad = {
+                  TrackpadRightClick = true;
+                  TrackpadTwoFingerFromRightEdgeSwipeGesture = 3;
+                };
+                iCal."TimeZone support enabled" = true;
+                loginwindow = {
+                  GuestEnabled = false;
+                  autoLoginUser = user;
+                };
+              };
             }
           )
           home-manager.darwinModules.home-manager
