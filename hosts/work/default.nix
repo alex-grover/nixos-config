e2b6e30ac85b15e1e1a71e2b74eb6875ec4b4c48
Alex Grover <hello@alexgrover.me>
2026-04-14 15:36:42 -0400

    Install codex and linear

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -1,5 +1,9 @@
 { lib, user, ... }:
 {
+  homebrew.casks = [
+    "linear-linear"
+  ];
+
   system.defaults.dock.persistent-apps = lib.mkOrder 1250 [
     "/Users/${user}/Applications/Home Manager Apps/Slack.app"
   ];
--- modules/home/work.nix
+++ modules/home/work.nix
@@ -2,6 +2,7 @@
 {
   home.packages = [
     pkgs.claude-code
+    pkgs.codex
     pkgs.nodejs_22
     pkgs.slack
     pkgs.zoom-us
