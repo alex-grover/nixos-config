4b22849f02eae191fa8b295940a3498b132deee9
Alex Grover <hello@alexgrover.me>
2026-07-09 15:12:34 -0400

    Bump pi version

--- pkgs/pi-coding-agent.nix
+++ pkgs/pi-coding-agent.nix
@@ -7,17 +7,17 @@
 
 buildNpmPackage rec {
   pname = "pi-coding-agent";
-  version = "0.80.3";
+  version = "0.80.5";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@earendil-works/pi-coding-agent/-/pi-coding-agent-${version}.tgz";
-    hash = "sha256-nrDCKnN3Cmcmvoa72FFc+4MRfxJmvPX66DvPyuk6bFI=";
+    hash = "sha256-5DZxrGLZQx10CCO7q8iCMkj4vSchV8Jz87nk1aONqHs=";
     postFetch = ''
       ${lib.getExe npm-lockfile-fix} $out/npm-shrinkwrap.json
     '';
   };
 
-  npmDepsHash = "sha256-xKmIXXx3RXCN8Z+vUSS6MXCaN7VXGRt0TCkKKsLCvHE=";
+  npmDepsHash = "sha256-k3RJrINSrU7/6dGHLpxMJJmjZE79lz2HTQdPFwzFvis=";
   dontNpmBuild = true;
   npmRebuildFlags = [ "--ignore-scripts" ];
 
