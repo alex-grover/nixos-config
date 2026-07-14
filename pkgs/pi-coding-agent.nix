8184cfdd6361c01036d7aff6b9f65ab6a65b7f3c
Alex Grover <hello@alexgrover.me>
2026-07-14 14:46:51 -0400

    Update pi

--- pkgs/pi-coding-agent.nix
+++ pkgs/pi-coding-agent.nix
@@ -7,17 +7,17 @@
 
 buildNpmPackage rec {
   pname = "pi-coding-agent";
-  version = "0.80.5";
+  version = "0.80.7";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@earendil-works/pi-coding-agent/-/pi-coding-agent-${version}.tgz";
-    hash = "sha256-5DZxrGLZQx10CCO7q8iCMkj4vSchV8Jz87nk1aONqHs=";
+    hash = "sha256-B0DrLZPYXTUAG8q13DKagSPJobgourXXOgVjMgaJGQ0=";
     postFetch = ''
       ${lib.getExe npm-lockfile-fix} $out/npm-shrinkwrap.json
     '';
   };
 
-  npmDepsHash = "sha256-k3RJrINSrU7/6dGHLpxMJJmjZE79lz2HTQdPFwzFvis=";
+  npmDepsHash = "sha256-877PUl4olE3M346p86z1+3YyDKJLqZ6RH/BsouIg52g=";
   dontNpmBuild = true;
   npmRebuildFlags = [ "--ignore-scripts" ];
 
