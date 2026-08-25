5fd84b9f00e053b21a5b5a38778f52d23a4abc16
Alex Grover <hello@alexgrover.me>
2026-08-25 11:13:19 -0400

    Remove Okta from homebrew

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -1,6 +1,6 @@
 { lib, ... }:
 {
-  homebrew.casks = [ "okta-verify" ];
+  homebrew.masApps."Okta Verify" = 490179405;
 
   system.defaults.dock.persistent-apps = lib.mkOrder 1250 [
     "/Applications/Slack.app"
